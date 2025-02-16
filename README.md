c8b6f28cc4d5685f9036be28029b10324a2bef06
Christian-Rui <chris2w0social@gmail.com>
2025-02-16 08:05:11 -0300

    feat: finalizar projeto do jogo do número secreto

---
+++ README.md
@@ -0,0 +1,45 @@
+![Descricao da sua imagem](https://repository-images.githubusercontent.com/768217367/3f5266ef-6e63-478a-a18f-f9f4ba280759)
+
+# Jogo do Número Secreto
+
+Um jogo simples para descobrir o número secreto, feito durante o curso de lógica de programação com JavaScript da Alura.
+
+## 🔨 Funcionalidades do projeto
+
+Nesse projeto, você deve tentar descobrir qual é número secreto, sendo ele, um número de 1 a 10. Na página, existe um input onde você pode inserir o valor para verificar se acertou o número ou se o número digita é menor ou maior que o número secreto. Ao acertar o número secreto, o programa irá te informar em quantas tentativas você descobriu. O programa possui leitura por voz de alguns textos que aparecem na tela. 
+
+![](./img/JogoNumeroSecretoCaptura.png)
+
+## ✔️ Técnicas e tecnologias utilizadas
+
+As técnicas e tecnologias utilizadas pra isso são:
+
+- `HTML e CSS`: Foi dado um layout base da pagina no curso em que esse projeto foi feito.
+-  `JavaScript`: A lógica do programa foi feita interamente com JavaScript.
+- `speechSynthesis`: Sistema de leitura de tela de alguns textos.
+- `select e options`: foi usado o sistema de seleção para o usuário ter a possibilidade de alterar o tipo de voz da leitura de tela
+
+<div>
+  <img src="https://img.shields.io/badge/HTML-239120?style-for-the-badge&logo-html5&logoColor-white">
+  <img src="https://img.shields.io/badge/CSS-239120?&style-for-the-badge&logo-css3&logoColor-white">
+  <img src="https://img.shields.io/badge/JavaScript-F2DF1E?style-for-the-badge&logo-javascript&logoColor-black">
+</div>
+
+## 📁 Acesso ao projeto
+
+Você pode [baixar o arquivo zip]().
+
+## 🛠️ Abrir e rodar o projeto
+
+Após baixar o projeto, você pode abrir com o Visual Studio Code para acessa-lo e modificar o código. Para isso, na tela inicial clique em:
+
+- **Open Folder** (ou alguma opção similar)
+- Procure o local onde o projeto está e o selecione (Caso o projeto seja baixado via zip, é necessário extraí-lo antes de procurá-lo)
+- Por fim clique em OK
+
+Para  abrir, basta clicar duas vezes no arquivo **index.html** que ele será aberto no seu navegador, e você poderá testar suas funcionalidades
+
+# Time
+
+| [<img loading="lazy" src="https://avatars.githubusercontent.com/u/113655013?v=4" width=115><br><sub>Christian Rui</sub>](https://github.com/Christian-Rui)
+| :---: |
---
+++ app.js
@@ -0,0 +1,149 @@
+let listaDeNumerosSorteados = [];
+let numeroMaximo = 10;
+let numeroSecreto = gerarNumeroAleatorio(numeroMaximo);
+let tentativas = 1;
+let informouVoz = false;
+let voices = [];
+let selectTag = document.getElementById("voices");
+let vozPadrao = 1;
+
+function gerarNumeroAleatorio(max) {
+  let numeroEscolhido;
+  let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+  if (quantidadeDeElementosNaLista === max) listaDeNumerosSorteados = [];
+
+  do {
+    numeroEscolhido = parseInt(Math.random() * max + 1);
+  } while (listaDeNumerosSorteados.includes(numeroEscolhido));
+
+  listaDeNumerosSorteados.push(numeroEscolhido);
+
+  return numeroEscolhido;
+}
+
+// Chama a função para esperar as vozes e prosseguir
+
+function criarOptionsVoices(voicesArray) {
+  let novaOption;
+  for (let i = 0; i < voicesArray.length; i++) {
+    novaOption = document.createElement("option");
+    novaOption.value = i;
+    novaOption.textContent = `${voicesArray[i].name}`;
+    selectTag.appendChild(novaOption);
+  }
+}
+
+// Função para esperar as vozes carregarem
+function esperarVoices() {
+  const interval = setInterval(() => {
+    voices = window.speechSynthesis.getVoices();
+    if (voices.length > 0) {
+      clearInterval(interval); // Para o intervalo quando as vozes estiverem carregadas
+      criarOptionsVoices(voices);
+      definirVozSelecionada();
+    }
+  }, 100); // Verifica a cada 100ms
+}
+
+// Função para definir a voz selecionada
+function definirVozSelecionada() {
+  let vozSelecionada = sessionStorage.getItem("vozSelecionada");
+  if (vozSelecionada) {
+    selectTag.value = vozSelecionada; // Define a voz selecionada
+    return;
+  }
+  selectTag.value = 1;
+  return;
+}
+
+esperarVoices();
+
+// Adiciona um evento para salvar a voz selecionada
+selectTag.addEventListener("change", function () {
+  window.speechSynthesis.cancel();
+  sessionStorage.setItem("vozSelecionada", selectTag.value); // Salva a voz selecionada
+  fazerLeitura(retornarTag("h1").textContent);
+  fazerLeitura(retornarTag("p").textContent);
+});
+
+function possibilitarLeitura(texto) {
+  window.speechSynthesis.cancel();
+  let i = 0;
+  if (!(voices.length > 0)) {
+    const interval = setInterval(() => {
+      if(voices.length > 0) {
+        clearInterval(interval);
+        fazerLeitura(texto); // Faz a leitura assim que as vozes estão disponíveis
+      }
+    }, 100);
+  } else {
+    fazerLeitura(texto);
+  }
+}
+
+function fazerLeitura(texto) {
+  if ("speechSynthesis" in window) {
+    let utterance = new SpeechSynthesisUtterance(texto);
+    utterance.lang = "pt-BR";
+    utterance.rate = 1.2;
+    // Aqui você pode escolher a voz desejada
+    utterance.voice = voices[selectTag.value];
+    if (!informouVoz) {
+      console.log(`Voz escolhida para leitura foi: ${voices[0].name}`);
+      informouVoz = true;
+    }
+    window.speechSynthesis.speak(utterance);
+  } else {
+    console.log("Web Speech API não suportada neste navegador.");
+  }
+}
+
+function retornarTag(tag) {
+  return document.querySelector(tag);
+}
+
+function exibirTextoNaTela(tag, texto) {
+  let campo = retornarTag(tag);
+  campo.innerHTML = texto;
+  // responsiveVoice.speak(texto, "Brazilian Portuguese Female", {rate: 1.2});
+  possibilitarLeitura(texto);
+}
+
+function exibirMensagemInicial() {
+  exibirTextoNaTela("h1", "Jogo do número secreto");
+  exibirTextoNaTela("p", `Escolha um número entre 1 e ${numeroMaximo}`);
+}
+
+exibirMensagemInicial();
+
+function verificarChute() {
+  let chute = parseInt(document.querySelector("input").value);
+  if (chute === numeroSecreto) {
+    exibirTextoNaTela("h1", "Acertou!");
+    let mensagemTentativas = `Você descobriu o número secreto em ${tentativas} tentativa${
+      tentativas > 1 ? "s" : ""
+    }!`;
+    exibirTextoNaTela("p", mensagemTentativas);
+    document.getElementById("reiniciar").removeAttribute("disabled");
+  } else {
+    tentativas++;
+    let mensagemDica = `O número secreto é ${
+      chute > numeroSecreto ? "menor" : "maior"
+    }`;
+    exibirTextoNaTela("p", mensagemDica);
+    limparCampo();
+  }
+}
+
+function limparCampo() {
+  chute = document.querySelector("input");
+  chute.value = "";
+}
+
+function reiniciarJogo() {
+  numeroSecreto = gerarNumeroAleatorio(numeroMaximo);
+  limparCampo();
+  tentativas = 1;
+  exibirMensagemInicial();
+  document.getElementById("reiniciar").setAttribute("disabled", true);
+}
